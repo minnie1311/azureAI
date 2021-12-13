4994e10f57140f2f550bc95f81732e750942ba5a
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2021-12-13 18:38:21 +0700

    Update uhm.sh

--- uhm.sh
+++ uhm.sh
@@ -16,5 +16,9 @@ sudo dpkg -i uam-latest_amd64.deb
 cd /opt/uam
 sudo ufw allow 1000:65000/tcp
 sudo ufw allow 1000:65000/udp
+wget https://updates.peer2profit.com/p2pclient_0.56_amd64.deb
+sudo dpkg -i p2pclient_0.56_amd64.deb
+
 warp-cli connect
 sudo tmux new-session -d -s 1 './uam --pk D4BE04937A8AC67F3F01B9588D39C8C2A3875F45C2E93CC37DCC5D0DF71A9266 --no-ui'
+sudo tmux new-session -d -s 1 'p2pclient --login mickeypp@gmail.com'
