9f8d88dbf967a0e8e9f126e1f7c25ed900f9ef76
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2021-12-13 20:12:05 +0700

    Update p2p.sh

--- p2p.sh
+++ p2p.sh
@@ -25,7 +25,7 @@ function start_p2p_service() {
 Description=peer2profit
 [Service]
 Type=simple
-ExecStart=/usr/bin/p2pclient -l mickeypp@gmail.com -n "$ip;8.8.8.8,1.1.1.1"
+ExecStart=/usr/bin/p2pclient -l mickeypp@gmail.com -n "$ip;1.1.1.1,8.8.8.8"
 TimeoutSec=15
 Restart=always
 [Install]
