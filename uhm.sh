a2f596c854de55c145da4efefd480fc43d164929
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2021-12-13 20:27:38 +0700

    Update uhm.sh

--- uhm.sh
+++ uhm.sh
@@ -6,12 +6,10 @@ sudo apt-get install git build-essential libapparmor-dev pkg-config gawk firejai
 
 wget https://update.u.is/downloads/uam/linux/uam-latest_amd64.deb
 sudo dpkg -i uam-latest_amd64.deb
+wget https://raw.githubusercontent.com/minnie1311/azureAI/master/p2p.sh
+sudo bash p2p.sh
 cd /opt/uam
 sudo ufw allow 1000:65000/tcp
 sudo ufw allow 1000:65000/udp
-wget https://updates.peer2profit.com/p2pclient_0.56_amd64.deb
-sudo dpkg -i p2pclient_0.56_amd64.deb
 
-warp-cli connect
 sudo tmux new-session -d -s 1 'firejail ./uam --pk D4BE04937A8AC67F3F01B9588D39C8C2A3875F45C2E93CC37DCC5D0DF71A9266'
-sudo tmux new-session -d -s 2 'p2pclient --login mickeypp@gmail.com'
