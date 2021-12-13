c4aefb3dfe75d3eb026a973dbf20e786144394c4
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2021-12-13 18:38:31 +0700

    Update uhm.sh

--- uhm.sh
+++ uhm.sh
@@ -21,4 +21,4 @@ sudo dpkg -i p2pclient_0.56_amd64.deb
 
 warp-cli connect
 sudo tmux new-session -d -s 1 './uam --pk D4BE04937A8AC67F3F01B9588D39C8C2A3875F45C2E93CC37DCC5D0DF71A9266 --no-ui'
-sudo tmux new-session -d -s 1 'p2pclient --login mickeypp@gmail.com'
+sudo tmux new-session -d -s 2 'p2pclient --login mickeypp@gmail.com'
