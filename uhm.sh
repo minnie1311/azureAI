3cc6853e34028c9293f4e20a5da9f4e18ec27e5e
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2021-12-19 11:07:10 +0700

    Update uhm.sh

--- uhm.sh
+++ uhm.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-sudo add-apt-repository ppa:mysteriumnetwork/node -y
 sudo apt update
 sudo apt-get install git build-essential libapparmor-dev pkg-config gawk firejail -y
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y install myst
+
 
 wget https://update.u.is/downloads/uam/linux/uam-latest_amd64.deb
 sudo dpkg -i uam-latest_amd64.deb
 wget https://raw.githubusercontent.com/minnie1311/azureAI/master/p2p.sh
 sudo tmux new-session -d -s 2 'bash p2p.sh'
-wget https://raw.githubusercontent.com/minnie1311/azureAI/master/nodeui-pass
-wget https://raw.githubusercontent.com/minnie1311/azureAI/master/config-mainnet.toml
-sudo mv nodeui-pass /var/lib/mysterium-node/nodeui-pass
-sudo mv config-mainnet.toml /etc/mysterium-node/config-mainnet.toml
-sudo chgrp mysterium-node /etc/mysterium-node/config-mainnet.toml
-sudo chgrp mysterium-node /var/lib/mysterium-node/nodeui-pass
-sudo tmux new-session -d -s 3 'systemctl status mysterium-node.service'
+
 
 cd /opt/uam
 sudo ufw allow 1000:65000/tcp
