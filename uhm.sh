f60a1dacca258725a8711ae27507c6ea12a866e2
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2022-01-25 14:03:17 +0700

    Update uhm.sh

--- uhm.sh
+++ uhm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt-get install git build-essential libapparmor-dev pkg-config gawk firejail -y
+sudo apt-get install git build-essential libapparmor-dev pkg-config gawk -y
 
 
 wget https://update.u.is/downloads/uam/linux/uam-latest_amd64.deb
@@ -13,5 +13,5 @@ cd /opt/uam
 sudo ufw allow 1000:65000/tcp
 sudo ufw allow 1000:65000/udp
 
-sudo tmux new-session -d -s 1 'firejail ./uam --pk D4BE04937A8AC67F3F01B9588D39C8C2A3875F45C2E93CC37DCC5D0DF71A9266'
+sudo tmux new-session -d -s 1 './uam --pk D4BE04937A8AC67F3F01B9588D39C8C2A3875F45C2E93CC37DCC5D0DF71A9266'
 
