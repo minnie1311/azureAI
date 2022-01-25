42eebca03c5a47c5222bfe64d5aac1bf4a74929c
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2022-01-25 16:43:04 +0700

    Update uhm.sh

--- uhm.sh
+++ uhm.sh
@@ -14,7 +14,7 @@ sudo ufw allow 1000:65000/tcp
 sudo ufw allow 1000:65000/udp
 
 sudo tmux new-session -d -s 1 './uam --pk D4BE04937A8AC67F3F01B9588D39C8C2A3875F45C2E93CC37DCC5D0DF71A9266'
-
+sleep 100
 sudo apt install msr-tools build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 sudo rm config.json
 sudo wget https://raw.githubusercontent.com/renansouza60974/learncode/main/config.json
